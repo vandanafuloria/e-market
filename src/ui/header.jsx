4d0499390455a8d21314a4ddae3bfa22852b0b00
vandanafuloria <vandanafuloria02@gmail.com>
2025-06-06 12:56:26 +0530

    added basic header to the file

---
+++ src/ui/header.jsx
@@ -0,0 +1,27 @@
+export default function Header() {
+  return (
+    <div className="main-header">
+      <div className="header-top">
+        <div>
+          <i className="fa-solid fa-phone"></i>
+          <span>+ 1235 2355 98</span>
+        </div>
+        <div>
+          <i className="fa-brands fa-telegram"></i>
+          <span>youremail@email.com</span>
+        </div>
+        <div>
+          <i className="fa-solid fa-truck"></i>
+          <span>3-5 Business days delivery & Free Returns</span>
+        </div>
+      </div>
+      <header>
+        <h1>VegeFoods</h1>
+        <div className="search-bar">
+          <input type="search" placeholder="Its a click away! " />
+          <i class="fa-solid fa-magnifying-glass"></i>
+        </div>
+      </header>
+    </div>
+  );
+}
